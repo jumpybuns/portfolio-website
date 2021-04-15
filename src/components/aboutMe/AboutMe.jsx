f65c25f81c6e38a468983f038a1a60122a572e4c
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-04-14 23:45:44 -0700

    add nav words

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import styles from './AboutMe.css';
 import Header from '../header/Header';
+import Triangle from './Triangle';
+import ethan from '../../../public/ethan.jpg';
 
 export default function AboutMe() {
   return (
     <>
       <Header />
-      <div className={styles.aboutMe}>
-        <h2>
+      <Triangle />
+      <div className={styles.background}>
+        <h2 className={styles.aboutMeBox}>
+          and have a unique perspective with a passion for creativity, fixing
+          bugs, problem solving and I love discussing new concepts and ideas. I
+          am a fun, friendly and patient person who can keep cool in stressful
+          situations and is able to relate and communicate well with clients and
+          coworkers alike.
+        </h2>
+        <h2 className={styles.aboutMeBox2}>
           I am focused and have a unique perspective with a passion for
           creativity, fixing bugs, problem solving and I love discussing new
           concepts and ideas. I am a fun, friendly and patient person who can
           keep cool in stressful situations and is able to relate and
           communicate well with clients and coworkers alike.
         </h2>
+
+        <img
+          className={styles.image}
+          alt="ethan"
+          src={ethan}
+          height="200px"
+          width="200px"
+        />
       </div>
     </>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,11 @@ import styles from './Header.css';
 export default function Header() {
   return (
     <div className={styles.header}>
-      <h1>Navigation Here</h1>
+      <h1 className={styles.homeNav}>Home</h1>
+      <h1 className={styles.abtMeNav}>About Me</h1>
+      <h1 className={styles.tsNav}>Tech Stack</h1>
+      <h1 className={styles.projNav}>Projects</h1>
+      <h1 className={styles.contNav}>Contact</h1>
     </div>
   );
 }
