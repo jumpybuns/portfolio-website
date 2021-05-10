2c74f6c3c4bacbdcac5529ed5c5c2c6115090d28
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-05-10 01:20:29 -0700

    i have to pee now

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Projects.css';
+import { Link } from 'react-router-dom';
 import aurora from '../../../public/aurora.jpg';
 import karaoke from '../../../public/karaoke.jpg';
 import chatable from '../../../public/chatable.jpg';
@@ -8,22 +9,33 @@ import Header from '../header/Header';
 
 export const Projects = () => {
   return (
-    <>
+    <div>
       <Header />
-      <div className={styles.container}>
-        <div className={styles.column1}>
-          <img src={aurora} alt="aurora synth" width="200"></img>
-          <div className={styles.column2}>
-            <img src={karaoke} alt="karaoke roulette" width="200"></img>
-          </div>
-          <div className={styles.column3}>
-            <img src={chatable} alt="chatable" width="200"></img>
-          </div>
-          <div className={styles.column4}>
-            <img src={bloom} alt="bloom" width="200"></img>
-          </div>
-        </div>
-      </div>
-    </>
+      <h1 className={styles.contact}>Contact</h1>
+      <a className={styles.button} download="Ethan Pierce Resume">
+        Aurora Synth
+      </a>
+      <Link
+        to={{ pathname: 'https://github.com/jumpybuns' }}
+        target="_blank"
+        style={{ textDecoration: 'none' }}
+      >
+        <h3 className={styles.github}>Karaoke Roulette</h3>
+      </Link>
+      <Link
+        to={{ pathname: 'https://mail.google.com/' }}
+        target="_blank"
+        style={{ textDecoration: 'none' }}
+      >
+        <h3 className={styles.email}>Chatable</h3>
+      </Link>
+      <Link
+        to={{ pathname: 'https://www.linkedin.com/in/ethanpiercepresents' }}
+        target="_blank"
+        style={{ textDecoration: 'none' }}
+      >
+        <h3 className={styles.linkedin}>Bloom Financial</h3>
+      </Link>
+    </div>
   );
 };
