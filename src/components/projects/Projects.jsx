40faef618b1e0249f90de917d1b9e28c22040ad7
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-05-13 10:29:22 -0700

    adjust projects images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,10 +8,14 @@ import Header from '../header/Header';
 
 const images = { aurora, karaoke, chatable, bloom };
 
-const auroraText = 'hello there';
-const karaokeText = 'karaoke jams';
-const chatableText = 'chatable jams';
-const bloomText = 'bloom jams';
+const auroraText =
+  'Beautiful interactive web browser synth built with vanilla JavaScript and Web Audio API. I implimented the series of audio nodes and created the different wave shapes and filters as well as used ChartJS to create the dynamic graph';
+const karaokeText =
+  'Social app utilizing YouTube API to create randomized karaoke videos. We used Javascript, React, SQL, Heroku, Netlify, CSS I created the API call, implemented the authorization, ran tests, and created the CRUD routes.';
+const chatableText =
+  'Instantaneous live chat app and chat rooms created with Socket.IO and Material-UI. I designed and created the messaging emits and broadcasts. I also wrote the User and Auth models/routes. ';
+const bloomText =
+  'Stock Trading learning tool using NewsAPI, Live Stock Ticker, Finance Widget and seemlessly integrating Alpaca Finance with your own portfolio. I setup the Oauth, created the landing page and built the entirety of the Alpaca integration.';
 const text = { auroraText, karaokeText, chatableText, bloomText };
 
 const auroraTitle = 'Aurora Synth';
