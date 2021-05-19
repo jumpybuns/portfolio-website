78a483bc24295ccc63f21f6e16d2b54827eb0d63
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-05-19 10:04:10 -0700

    header and name spacing

--- src/components/landing/Name.jsx
+++ src/components/landing/Name.jsx
@@ -7,6 +7,9 @@ export default function Name() {
       <div className={styles.atContainer}>
         <h1 className={styles.firstName}>Ethan</h1>
         <h1 className={styles.lastName}>Pierce</h1>
+        <p className={styles.description}>
+          Freelance Fullstack Developer based in Portland, OR
+        </p>
       </div>
     </div>
   );
