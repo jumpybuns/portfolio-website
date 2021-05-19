558f046cb351babc7799bc83cf630badaaa36f28
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-05-18 18:15:21 -0700

    add backtotop arrow

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,41 +12,45 @@ export const Contact = () => {
     <>
       <div id="top">
         <Header />
-        <div id="contact">
+        <div className={styles.container1}>
           <h1 className={styles.contact}>Contact</h1>
-          <a
-            className={styles.button}
-            href={resume}
-            download="Ethan Pierce Resume"
-          >
-            Download Resume
-          </a>
-          <Link
-            to={{ pathname: 'https://github.com/jumpybuns' }}
-            target="_blank"
-            style={{ textDecoration: 'none' }}
-          >
-            <h3 className={styles.github}>GitHub</h3>
-          </Link>
-          <Link
-            to={{ pathname: 'https://mail.google.com/' }}
-            target="_blank"
-            style={{ textDecoration: 'none' }}
-          >
-            <h3 className={styles.email}>Email</h3>
-          </Link>
-          <Link
-            to={{ pathname: 'https://www.linkedin.com/in/ethanpiercepresents' }}
-            target="_blank"
-            style={{ textDecoration: 'none' }}
-          >
-            <h3 className={styles.linkedin}>LinkedIn</h3>
-          </Link>
-        </div>
-        <div className={styles.arrow}>
-          <HashLink smooth to={'/contact#top'}>
-            <img src={upArrow} alt="" width="20px" />
-          </HashLink>
+          <div id="contact">
+            <a
+              className={styles.button}
+              href={resume}
+              download="Ethan Pierce Resume"
+            >
+              Download Resume
+            </a>
+            <Link
+              to={{ pathname: 'https://github.com/jumpybuns' }}
+              target="_blank"
+              style={{ textDecoration: 'none' }}
+            >
+              <h3 className={styles.github}>GitHub</h3>
+            </Link>
+            <Link
+              to={{ pathname: 'https://mail.google.com/' }}
+              target="_blank"
+              style={{ textDecoration: 'none' }}
+            >
+              <h3 className={styles.email}>Email</h3>
+            </Link>
+            <Link
+              to={{
+                pathname: 'https://www.linkedin.com/in/ethanpiercepresents',
+              }}
+              target="_blank"
+              style={{ textDecoration: 'none' }}
+            >
+              <h3 className={styles.linkedin}>LinkedIn</h3>
+            </Link>
+          </div>
+          <div className={styles.arrow}>
+            <HashLink smooth to={'/contact#top'}>
+              <img src={upArrow} alt="" width="20px" />
+            </HashLink>
+          </div>
         </div>
       </div>
     </>
