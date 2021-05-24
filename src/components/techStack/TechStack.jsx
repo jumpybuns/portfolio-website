11bee19ee9244847e6603476e627b3656796b95d
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-05-24 13:52:15 -0700

    add favicon

--- src/components/techStack/TechStack.jsx
+++ src/components/techStack/TechStack.jsx
@@ -6,17 +6,17 @@ export const TechStack = () => {
   return (
     <div id="techstack" className={styles.gridContainer}>
       <h1 className={styles.techStack}>TECH STACK</h1>
-      <div className={styles.languages}>
-        <h2 className={styles.techs}>Languages</h2>
+      <div className={styles.techs}>
+        <h2>Languages</h2>
         <pre>Javascript HTML CSS Python</pre>
 
-        <h2 className={styles.techs}>Front End</h2>
+        <h2>Front End</h2>
         <pre>React D3 Material-UI Netlify</pre>
 
-        <h2 className={styles.techs}>Back End</h2>
+        <h2>Back End</h2>
         <pre>Node.JS PostgreSQL Express BCrypt</pre>
 
-        <h2 className={styles.techs}>Dev Tools</h2>
+        <h2>Dev Tools</h2>
         <pre>Heroku Postman PGAdmin</pre>
       </div>
       <Circles />
