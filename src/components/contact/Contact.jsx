2c31059a9100ec80c8cecce61bfe1791004d8b4f
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-06-12 22:19:58 -0700

    looks good on iphone

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,10 +5,15 @@ import styles from './Contact.css';
 import resume from '../../../public/resume.pdf';
 import upArrow from '../../../public/upArrow.png';
 import { Circles } from './Circles';
+import { style } from 'd3-selection';
 
 export const Contact = () => {
   return (
     <div id="contact" className={styles.container}>
+      <Circles />
+      <div className={style.contactMe}>
+        <h1 className={styles.contact}>Contact Me</h1>
+      </div>
       <div className={styles.links}>
         <Link
           to={{ pathname: 'https://github.com/jumpybuns' }}
@@ -34,18 +39,29 @@ export const Contact = () => {
           <p>LinkedIn</p>
         </Link>
       </div>
-      <a
-        className={styles.myButton}
-        href={resume}
-        download="Ethan Pierce Resume"
-      >
-        Download Resume
-      </a>
-      <h1 className={styles.contact}>Contact Me</h1>
-      <Circles />
-      <HashLink className={styles.arrow} smooth to={'#top'}>
-        <img src={upArrow} alt="" width="20px" />
-      </HashLink>
+      <div className={style.downloadButton}>
+        <a
+          className={styles.myButton}
+          href={resume}
+          download="Ethan Pierce Resume"
+        >
+          Download Resume
+        </a>
+      </div>
+      <div className={style.topArrow}>
+        <HashLink className={styles.arrow} smooth to={'#top'}>
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+          <img src={upArrow} alt="" width="10%" />
+        </HashLink>
+      </div>
     </div>
   );
 };
