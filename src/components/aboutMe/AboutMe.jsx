03ef0aee79048629594ee8599487d6a0d73d74bf
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-08-05 12:04:37 -0700

    about me page redo

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -7,32 +7,32 @@ import ethan from '../../../public/ethan.jpg';
 export const AboutMe = () => {
   return (
     <div id="aboutme" className={styles.container}>
-      <h1 className={styles.aboutMe}>ABOUT ME</h1>
-      <h3 className={styles.text}>
-        Focused with a unique perspective and a passion for creativity, fixing
-        bugs, problem solving. I love discussing new concepts and interesting
-        ideas.
-      </h3>
+      <div className={styles.aboutMeBox}>
+        <h1 className={styles.aboutMe}>ABOUT ME</h1>
+      </div>
+      <div className={styles.text}>
+        <h3>
+          Focused with a unique perspective and a passion for creativity, fixing
+          bugs, problem solving. I love discussing new concepts and interesting
+          ideas.
+        </h3>
+      </div>
       <div className={styles.profile}>
         <Link
           to={{ pathname: 'https://github.com/jumpybuns' }}
           target="_blank"
           style={{ textDecoration: 'none' }}
         >
-          <img
-            className={styles.image}
-            alt="ethan"
-            src={ethan}
-            style={{ height: '200px', width: '200px' }}
-          />
+          <img className={styles.image} alt="ethan" src={ethan} />
         </Link>
       </div>
-
-      <h3 className={styles.text2}>
-        I am a fun, friendly and patient person who can keep cool in stressful
-        situations and is able to relate and communicate well with clients and
-        coworkers alike.
-      </h3>
+      <div className={styles.text2}>
+        <h3>
+          I am a fun, friendly and patient person who can keep cool in stressful
+          situations and is able to relate and communicate well with clients and
+          coworkers alike.
+        </h3>
+      </div>
     </div>
   );
 };
